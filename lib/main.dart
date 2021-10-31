77cd340fdfc895a9ec248bc5ea7ec32d01e67cd1
tim9819 <33243251+tim9819@users.noreply.github.com>
2021-11-01 01:35:46 +0900

    타이틀업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '어플이름',
+      title: '',
       home: MyClass(),
     );
   }
